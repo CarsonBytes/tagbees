63c082c91f4106c44c5911684d37b710a4ef1009
Carson Ng <carsonng2000@gmail.com>
2012-12-23 21:45:59 +0100

    added browser title and breadcrumbs

--- application/controllers/NewEventController.php
+++ application/controllers/NewEventController.php
@@ -5,12 +5,27 @@ class NewEventController extends Zend_Controller_Action
     public function preDispatch(){
     }
     
+    public function init()
+    {
+        $this->view->headTitle('Account - Create Event');
+    }
+    
     public function indexAction()
     {
+        Common::getSession()->nav=array(
+            'Home' => '/',
+            'Create Event' => null
+        );
+        
     }
     
     public function advancedAction()
     {
+        Common::getSession()->nav=array(
+            'Home' => '/',
+            'Create Event (Advanced)' => null
+        );
+        
         
     }
 }
--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -6,8 +6,20 @@ class UserController extends Zend_Controller_Action
        
     }
     
+    public function init()
+    {
+        $this->view->headTitle($this->_request->getParam('username').'\'s Profile - Tagbees');
+    }
+    
     public function indexAction()
     {
+        
+        Common::getSession()->nav=array(
+            'Home' => '/',
+            $this->_request->getParam('username').'\'s Profile' => null
+        );
+        
+        $this->view->username = $this->_request->getParam('username');
         
     }
 }
