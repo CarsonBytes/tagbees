bad205fb16747af3018d37f0d05360fd2f9e8cc7
Carson Ng <carsonng2000@gmail.com>
2013-03-28 03:50:39 +0100

    renamed page to feeds folder, organising event detail scss

--- application/controllers/EventController.php
+++ application/controllers/EventController.php
@@ -8,19 +8,27 @@ class EventController extends Zend_Controller_Action
     
     public function indexAction()
     {
-        if ($this->slug_name == 'new' || $this->slug_name == 'new_advanced'){
-            $this->redirect('event/new');
+        
+        if ($this->_request->getParam('slug_name')==""){
+        }else{
+            $itemService=new Service_Item();
+            if ($this->_request->getParam('tree_ref') != NULL) {
+                Zend_Registry::set('tree_ref', explode(',', $this->_request->getParam('tree_ref')));
+            }
+            $result=
+                $itemService->getItemBySlugName(
+                    urldecode($this->_request->getParam('slug_name'))
+                );
+            if ($result){
+                $this->view->item=$result['item'];
+                $this->view->have_gallery=1;
+                $this->view->related_categories=$result['related_categories'];
+                $this->view->result=1;
+                
+                $this->view->headTitle($result['item']->name);
+            }else{
+                $this->view->result=-1;
+            }
         }
-        
-    }
-    
-    public function newAction()
-    {
-        
-    }
-    
-    public function newAdvancedAction()
-    {
-        
     }
 }
