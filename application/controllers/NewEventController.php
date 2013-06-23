d27f0438a202ca6e779f841b38bd8aa5e9b48c03
Carson Ng <carsonng2000@gmail.com>
2013-06-23 23:13:23 +0200

    finetune timepicker, and jquery dialog box

--- application/controllers/NewEventController.php
+++ application/controllers/NewEventController.php
@@ -1,7 +1,6 @@
 <?php
-class NewEventController extends Zend_Controller_Action
-{
-    
+class NewEventController extends Zend_Controller_Action {
+
   public function preDispatch() {
     if (!Zend_Auth::getInstance() -> hasIdentity()) {
       $this -> _redirect('/auth/login?redirect=' . urlencode($this -> getRequest() -> REQUEST_URI));
@@ -9,28 +8,38 @@ class NewEventController extends Zend_Controller_Action
     }
   }
 
-    
-    public function init()
-    {
-        $this->view->headTitle('Tagbees - Create Event');
+  public function init() {
+    $this -> view -> headTitle('Tagbees - Create Event');
+
+    if (!isset($_SESSION['tree_json']) || $_SESSION['tree_json'] == '[]') {
+      $categoryService = new Service_Tree();
+      $category = $categoryService -> get(false);
+
+      $used_cats = array();
+      $js = array();
+      foreach ($category as &$cat) {
+        if (!in_array($cat['category_ids'], $used_cats)) {
+          $js[] = $cat['category_ids'];
+          $js[] = array($cat['id'], $cat['name']);
+          $used_cats[] = $cat['category_ids'];
+        } else {
+          $key = array_search($cat['category_ids'], $js);
+          $js[$key + 1][] = $cat['id'];
+          $js[$key + 1][] = $cat['name'];
+        }
+      }
+      $_SESSION['tree_json'] = json_encode($js);
     }
-    
-    public function indexAction()
-    {
-        Common::getSession()->nav=array(
-            'Home' => '/',
-            'Create Event' => null
-        );
-        
-    }
-    
-    public function advancedAction()
-    {
-        Common::getSession()->nav=array(
-            'Home' => '/',
-            'Create Event (Advanced)' => null
-        );
-        
-        
-    }
+  }
+
+  public function indexAction() {
+    Common::getSession() -> nav = array('Home' => '/', 'Create Event' => null);
+
+  }
+
+  public function advancedAction() {
+    Common::getSession() -> nav = array('Home' => '/', 'Create Event (Advanced)' => null);
+
+  }
+
 }
