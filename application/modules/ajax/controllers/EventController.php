6e0e34108663b0b16b452c6f6984c104c5b93aef
Carson Ng <carsonng2000@gmail.com>
2013-12-27 06:06:40 +0100

    [bookmark_popup] bug fixes for reminder fields reading and saving

--- application/modules/ajax/controllers/EventController.php
+++ application/modules/ajax/controllers/EventController.php
@@ -30,25 +30,9 @@ class Ajax_EventController extends Zend_Controller_Action
       if (!Zend_Auth::getInstance()->hasIdentity()){
           return false;
       } else{
-        //echo '<pre>';var_dump($this->params);echo '</pre>';die();
-        $prefix = 'event_reminder_';
-        /*$data = array(
-          'item_id' => $this->params[$prefix.'event_id'],
-          'title' => $this->params[$prefix.'title'],
-          'description' => $this->params[$prefix.'description'],
-          'tags' => $this->params[$prefix.'tags'],
-          'has_email_alarm' => in_array('email', $this->params[$prefix.'options_type']) ? 1 : 0,
-          'has_popup_alarm' => in_array('popup', $this->params[$prefix.'options_type']) ? 1 : 0,
-          'has_mobile_alarm' => in_array('mobile', $this->params[$prefix.'options_type']) ? 1 : 0,
-          'alarm_time' => json_encode(
-            array(
-              'time'=>$this->params[$prefix.'options_type_time'],
-              'time_unit'=>$this->params[$prefix.'options_type_time_unit']
-            )
-          )
-        );*/
         $eventService=new Service_Event();
         $array['result'] = $eventService->addReminder($this->params);
+        $array['data'] = $eventService->getReminder($this->params['event_reminder_item_id']);
       }
         
       $this->_helper->json($array);
