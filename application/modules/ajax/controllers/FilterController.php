fe986935c068130e3742877d2d954957fa93db2e
Carson Ng <carsonng2000@gmail.com>
2014-06-22 11:09:12 +0200

    [TAG-60] fixed search button save error on empty string on date fields

--- application/modules/ajax/controllers/FilterController.php
+++ application/modules/ajax/controllers/FilterController.php
@@ -10,20 +10,54 @@ class Ajax_FilterController extends Zend_Controller_Action
     $this->userSavedLocationService=new Service_UserSavedFilter();
   }
   public function saveAction(){
-    $result=$this->userSavedLocationService->save(
-        $this->params['name'], 
-        $this->params['q'],
-        //$this->params['sort_by'], 
-        $this->params['is_match_location'], 
-        $this->params['is_match_interest'], 
-        $this->params['place_lat'], 
-        $this->params['place_lng'], 
-        $this->params['radius'],
-        $this->params['begin_date'], 
-        $this->params['end_date'], 
-        $this->params['override']
+    $filters = array(
+      'is_match_location'   => 'Digits',
+      'is_match_interest'   => 'Digits',
+      'override'   => 'Digits',
+      'begin_time'=>new Zend_Filter_Null(Zend_Filter_Null::STRING),
+      'end_time'=>new Zend_Filter_Null(Zend_Filter_Null::STRING)
     );
-  $this->_helper->json(array('result'=>$result));
+    $validators = array(
+        'override'   => array(),
+        'name'   => array(),
+        'q'   => array(),
+        'is_match_location'   => array(),
+        'is_match_interest'   => array(),
+        'place_lat'   => array(new Zend_Validate_Float('en_US')),
+        'place_lng'   => array(new Zend_Validate_Float('en_US')),
+        'radius'   => array(new Zend_Validate_Float('en_US')),
+        'begin_date' => array(
+          new Zend_Validate_Date(array('format' => 'YYYY-MM-DD'))
+        ),
+        'end_date' => array(
+          new Zend_Validate_Date(array('format' => 'YYYY-MM-DD'))
+        ),
+    );
+    $options = array(
+      'allowEmpty' => true
+    );
+        
+    $input = new Zend_Filter_Input($filters, $validators,$this->params, $options);
+      
+    if ($input->hasInvalid() || $input->hasMissing()) {
+        $result = $input->getMessages();
+      }else{
+        $input_vals = $input->getEscaped();
+        $result=$this->userSavedLocationService->save(
+          $input_vals['name'], 
+          $input_vals['q'],
+          //$input_vals['sort_by'], 
+          $input_vals['is_match_location'], 
+          $input_vals['is_match_interest'], 
+          $input_vals['place_lat'], 
+          $input_vals['place_lng'], 
+          $input_vals['radius'],
+          $input_vals['begin_date'], 
+          $input_vals['end_date'], 
+          $input_vals['override']
+        );
+    }
+    $this->_helper->json(array('result'=>$result));
   }
   public function deleteAction(){
     $result=$this->userSavedLocationService->delete(
