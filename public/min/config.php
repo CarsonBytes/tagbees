ca06683f5943bb53a260b1133158219bdb0c8b11
Carson Ng <carsonng2000@gmail.com>
2014-06-25 15:15:23 +0200

    [TAG-87] fixed email settings on staging server

--- public/min/config.php
+++ public/min/config.php
@@ -59,8 +59,8 @@ $min_cachePath = realpath(dirname(__FILE__)) . "/../../tmp";
  * To use APC/Memcache/ZendPlatform for cache storage, require the class and
  * set $min_cachePath to an instance. Example below:
  */
-require dirname(__FILE__) . '/lib/Minify/Cache/APC.php';
-$min_cachePath = new Minify_Cache_APC();
+//require dirname(__FILE__) . '/lib/Minify/Cache/APC.php';
+//$min_cachePath = new Minify_Cache_APC();
 
 
 /**
